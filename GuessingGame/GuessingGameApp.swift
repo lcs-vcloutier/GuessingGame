a97c672201e22f3c1da744f93643fcbb24edef7e
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-17 14:49:12 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Cloutier, Vincent on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
