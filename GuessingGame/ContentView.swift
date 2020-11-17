373c96ae2769cd8a1162259e4024a3efdd354bbf
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-17 15:08:56 -0500

    got title to be navigation title

--- GuessingGame/ContentView.swift
+++ GuessingGame/ContentView.swift
@@ -11,35 +11,39 @@ struct ContentView: View {
     let target = Int.random(in: 1..<100)
     @State private var guess = ""
     @State var alert: Bool = false
-
+    
     var body: some View {
-        VStack{
-            Text("Guessing Game")
-            Text("I'm thinking of a number between 1 and 100")
-            TextField("Input guess here", text: $guess)
-                .keyboardType(.decimalPad)
-
-            Button(action: {checkGuess()}, label: {
-                Text("Guess")
-            })
-            Text("You guessed \(guess)")
+        NavigationView {
+            VStack {
+                Text("I'm thinking of a number between 1 and 100")
+                TextField("Input guess here", text: $guess)
+                    .keyboardType(.decimalPad)
+                
+                Button(action: {checkGuess()}, label: {
+                    Text("Guess")
+                })
+                Text("You guessed \(guess)")
+            }
+            .navigationTitle("Guessing Game")
+            .padding()
         }
-        .alert(isPresented: $alert, content: {
-            Alert(title: Text("Guessing Game"), message: Text("Error Reason"), dismissButton: .default(Text("OK")))
-        })
+
+//    .alert(isPresented: $alert, content: {
+//            Alert(title: Text("Guessing Game"), message: Text("\(msg)"), dismissButton: .default(Text("OK")))
+//        })
     }
     func checkGuess() {
         let guess2 = Int(guess)!
-        let 
+        var msg = ""
         alert = true
         if guess2 == target {
-            
+            msg = "You Got It!!!!"
         }
         else if guess2 > target {
-            
+            msg = "Guess lower"
         }
         else if guess2 < target {
-            
+            msg = "Guess lower"
         }
     }
 }
